d281ed865393af40aeebea34a1c955561b54edc0
Sonuu.Chowdhury <choudhurysonu2004@gmail.com>
2025-04-03 14:13:04 +0530

    qr reading feature added

--- Frontend/src/Pages/Attendance Admin/AttendnaceAdmin.jsx
+++ Frontend/src/Pages/Attendance Admin/AttendnaceAdmin.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import './AttendnaceAdmin.css';
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowsRotate, faTrash, faQrcode } from '@fortawesome/free-solid-svg-icons';
+import QrScanner from 'qr-scanner'
 
 export default function AttendanceAdminPage() {
   const navigate = useNavigate();
@@ -12,8 +13,32 @@ export default function AttendanceAdminPage() {
   const [PresentTeams, setPresentTeams] = useState([]);
   const [AbsentTeams, setAbsentTeams] = useState([]);
   const [expandedTeam, setExpandedTeam] = useState(null);
+
+  const [file,setFile]=useState(null)
   const [isScanning, setIsScanning] = useState(false);
   const [scannedData, setScannedData] = useState(null);
+
+  const handleFileChange = async (e) => {
+    if (e.target.files.length > 0) {
+      const imageFile = e.target.files[0];
+      const imageUrl = URL.createObjectURL(imageFile);
+      setFile(imageUrl); // Update the state for UI
+  
+      try {
+        const QRresponse = await QrScanner.scanImage(imageFile, { returnDetailedScanResult: true });
+        if (QRresponse) {
+          console.log(QRresponse.data);
+          setScannedData(QRresponse.data);
+        } else {
+          alert("No QR Found");
+        }
+      } catch (error) {
+        console.error("QR Scanning Error:", error);
+        alert("Unsupported image type or no QR code detected.");
+      }
+    }
+  };
+  
 
   useEffect(() => {
     const present = participantDetails.filter((team) => team.isPresent === true);
@@ -75,10 +100,29 @@ export default function AttendanceAdminPage() {
             Reset <FontAwesomeIcon icon={faTrash} />
           </button>
         </div>
-        <button className='ScanQRButton' onClick={() => setIsScanning(true)}>
-          Scan <FontAwesomeIcon icon={faQrcode} />
+        <button className='ScanQRButton' onClick={() => setIsScanning((val)=>!val)}>
+        {isScanning ? "Cancel" : <span>Scan <FontAwesomeIcon icon={faQrcode} /></span>}
         </button>
       </div>
+
+      {isScanning? <div className="ScannerArea">
+        <div className="ScanerAreaImageArea">
+          {file?
+          <img className='ScannerQRImage' src={file} alt="QR Image" />: <label htmlFor="qrinput" className="ScannerAreaImageAddArea">
+              Click to add image
+          </label>}
+          <input type="file" id="qrinput" onChange={handleFileChange}/>
+        </div>
+        <div className="ScannerControl">
+          <span className="ScannerControlMobile">
+            {scannedData} 
+          </span>
+          <button className="ScannerControlAddButton" disabled={!file}>
+            ADD
+          </button>
+        </div>
+      </div>
+      :null}
 
     </>
   );
